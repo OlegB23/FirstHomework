fa031d00d4a8f0d60b11afa168e85d6d2898fcae
OlegB23 <iviyx68kvj>
2018-06-06 12:15:57 +0300

    V2

--- src/test/java/firstHomework/FindArticle.java
+++ src/test/java/firstHomework/FindArticle.java
@@ -3,6 +3,7 @@ package firstHomework;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,18 +13,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindArticle {
 
+    WebDriver chrome;
+
     private final String HOMEPAGE = "http://rus.delfi.lv/";
 
-    private String articleName = "Школы переводят";
+    private String articleName = "Синоптики обещают";
+
+
+
+    @Before
+
+    public void createChromeDriver() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/oleg/Desktop/QA2/Drivers/chromedriver");
+
+        chrome = new ChromeDriver();
+
+    }
+
+
+
+    @After
+
+    public void closeWebsite() {
+
+        chrome.close();
+
+    }
+
 
 
     @Test
     public void openWebsite() {
 
-
-        System.setProperty("webdriver.chrome.driver", "/Users/oleg/Desktop/QA2/Drivers/chromedriver");
-
-        WebDriver chrome = new ChromeDriver();
 
         chrome.manage().window().maximize();
         chrome.get(HOMEPAGE);
